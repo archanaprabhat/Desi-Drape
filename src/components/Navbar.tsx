311ebdb0d540c46c2cff990d44f85db10641d982
archanaprabhat <archanaprabhath2002@gmail.com>
2025-08-18 17:15:13 +0530

    Add Prettier for code formatting; enhance Menu and Navbar components with improved layout and functionality.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,18 @@
-import Link from "next/link"
-import Menu from "./Menu"
+import Link from "next/link";
+import Menu from "./Menu";
 
 const Navbar = () => {
-    return(
-        <div className="h-20 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
-            {/* MOBILE */}
-            <Link href="/">Desi Drape</Link>
-            <Menu />
+  return (
+    <div className='h-20 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 relative'>
+      {/* MOBILE */}
+      <div className=' h-full flex justify-between items-center'>
+        <Link href='/'>
+          <div className=' text-2xl tracking-wider'>Desī Drapē</div>
+        </Link>
+        <Menu />
+      </div>
+    </div>
+  );
+};
 
-        </div>
-    )
-}
-
-export default Navbar
+export default Navbar;
