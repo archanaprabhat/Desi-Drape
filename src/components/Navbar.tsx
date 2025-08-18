270b2feaafd153a6f2de7230c9deddab87960ee2
archanaprabhat <archanaprabhath2002@gmail.com>
2025-08-18 16:22:29 +0530

    Update layout to include Navbar and Footer components; modify metadata description to reflect TypeScript usage.

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link"
+import Menu from "./Menu"
+
+const Navbar = () => {
+    return(
+        <div className="h-20 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
+            {/* MOBILE */}
+            <Link href="/">Desi Drape</Link>
+            <Menu />
+
+        </div>
+    )
+}
+
+export default Navbar
