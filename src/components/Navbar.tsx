9bbea9f61b2fecfb4ac44b81b3f58415e96a964a
archanaprabhat <archanaprabhath2002@gmail.com>
2025-08-19 12:43:59 +0530

    Refactor Navbar component to include SearchBar and NavIcons; adjust layout for mobile and larger screens.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,16 @@
 import Link from "next/link";
 import Image from "next/image";
 import Menu from "./Menu";
+import SearchBar from "./SearchBar";
+import NavIcons from "./NavIcons";
 
 const Navbar = () => {
   return (
     <div className='h-20 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 relative'>
       {/* MOBILE */}
-      <div className=' h-full flex justify-between items-center'>
+      <div className=' h-full flex justify-between items-center md:hidden'>
         <Link href='/'>
-          <div className=' text-2xl tracking-wider'>Desī Drapē</div>
+          <div className='font-extralight text-2xl tracking-wider'>Desī Drapē</div>
         </Link>
         <Menu />
       </div>
@@ -16,13 +18,16 @@ const Navbar = () => {
       <div className='hidden md:flex items-center justify-between gap-8 h-full'>
         {/* LEFT */}
         <div className='w-1/3'>
-          <Link href='/'>
+          <Link href='/' className="flex justify-center gap-3">
             <Image src='/logo.png' alt='Logo' width={24} height={24} />
-            <div className="text-2xl tracking-wider">Desī Drapē</div>
+            <div className="font-extralight text-2xl tracking-wider">Desī Drapē</div>
           </Link>
         </div>
         {/* RIGHT */}
-        <div className='w-2/3'></div>
+        <div className='w-2/3 flex items-center justify-between gap-8'>
+        <SearchBar />
+        <NavIcons />
+        </div>
       </div>
     </div>
   );
---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,9 @@
+const SearchBar = () => {
+    return (
+      <form action="">
+        <input type="text" placeholder="Search" />
+      </form>
+    )
+  }
+  
+  export default SearchBar
